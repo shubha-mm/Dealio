a2923043a796980a2cb6c5879b5f5c982aac5651
shubha-mm <167985010+shubha-mm@users.noreply.github.com>
2024-11-14 14:07:16 +0530

    commit

--- src/app/categories/categories.page.ts
+++ src/app/categories/categories.page.ts
@@ -1,38 +1,24 @@
-// categories.component.ts
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-categories',
-  templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss'],
+  templateUrl: './categories.page.html',
+  styleUrls: ['./categories.page.scss'],
 })
-export class CategoriesComponent {
+export class CategoriesPage {
   categories = [
-    {
-      title: 'Fruits & Vegetables',
-      items: [
-        { name: 'Fresh Vegetables', image: 'assets/images/fresh_vegetables.jpg' },
-        { name: 'Fresh Fruits', image: 'assets/images/fresh_fruits.jpg' },
-        { name: 'Herbs & Seasonings', image: 'assets/images/herbs_seasonings.jpg' },
-      ],
-    },
-    {
-      title: 'Bakery, Cakes & Dairy',
-      items: [
-        { name: 'Bakery Snacks', image: 'assets/images/bakery_snacks.jpg' },
-        { name: 'Breads & Buns', image: 'assets/images/breads_buns.jpg' },
-        { name: 'Cakes & Pastries', image: 'assets/images/cakes_pastries.jpg' },
-      ],
-    },
-    {
-      title: 'Breakfast, Dips & Spreads',
-      items: [
-        { name: 'Oats & Muesli', image: 'assets/images/oats_muesli.jpg' },
-        { name: 'Tomato Ketchup', image: 'assets/images/tomato_ketchup.jpg' },
-        { name: 'Chutney & Powder', image: 'assets/images/chutney_powder.jpg' },
-      ],
-    },
+    { name: 'Electronics', image: 'https://tse4.mm.bing.net/th?id=OIP.BGwhDC-5eqhHHqZkDPDMygHaE7&pid=Api&P=0&h=180', route: '/electronics' },
+    { name: 'Fashion', image: 'https://tse4.mm.bing.net/th?id=OIP.BGwhDC-5eqhHHqZkDPDMygHaE7&pid=Api&P=0&h=180', route: '/fashion' },
+    { name: 'Beauty', image: 'https://tse4.mm.bing.net/th?id=OIP.BGwhDC-5eqhHHqZkDPDMygHaE7&pid=Api&P=0&h=180', route: '/home-appliances' },
+    { name: 'sport', image: 'https://tse4.mm.bing.net/th?id=OIP.BGwhDC-5eqhHHqZkDPDMygHaE7&pid=Api&P=0&h=180', route: '/books' },
+    { name: 'food', image: 'https://tse4.mm.bing.net/th?id=OIP.BGwhDC-5eqhHHqZkDPDMygHaE7&pid=Api&P=0&h=180', route: '/toys' },
+    // Add more categories as needed
   ];
 
-  constructor() {}
+  constructor(private navCtrl: NavController) {}
+
+  navigateToCategory(route: string) {
+    this.navCtrl.navigateForward(route);
+  }
 }
